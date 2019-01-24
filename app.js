970e1db80e5948242e68ee91d6bd6e8b92cbfa0c
Lior Rabinovich <lior.rabinovich@gthsoftware.com>
2019-01-24 19:24:04 +0200

    Connect to mongo db

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
 
+const Event = require('./models/event');
+
 const app = express();
 
 const events = [];
@@ -46,24 +48,39 @@ app.use(
             return events;
         },
         createEvent: (args) => {
-            const event = {
-                _id: Math.random().toString(),
+            console.log('sgfghfda');
+            const nowString = new Date().toISOString();
+            const event = new Event({
                 title: args.eventInput.title,
                 description: args.eventInput.description,
                 price: +args.eventInput.price,
-                date: new Date().toISOString()
-            }
-            events.push(event);
-            return event;
+                date: new Date(nowString)
+            })
+            return event
+                .save()
+                .then((result)=>{
+                    console.log('sgfghfda2');
+                    console.log(result);
+                    return {...result._doc};
+                }).catch((err => {
+                    console.log(err);
+                    throw err;
+                })
+            );
         }
     },
     graphiql: true
 }))
 
-// mongoose.connect(
-//     `mongodb+srv://${process.env.MONGO_USER}:<${process.env.MONGO_PASSWORD}>@cluster0-gdutn.mongodb.net/test?retryWrites=true`
-// ).then(()=>{
-// }).catch(err => {
-//     console.log(err);
-// })
-app.listen(3000)
+console.log(`mongodb+srv://${process.env.MONGO_USER}:<${process.env.MONGO_PASSWORD}>@cluster0-gdutn.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`);
+
+mongoose.connect(`mongodb+srv://cluster0-gdutn.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`,{
+    user: process.env.MONGO_USER,
+    pass: process.env.MONGO_PASSWORD
+}).then(()=>{
+    const port = 3000;
+    console.log(`listen port ${port}`);
+    app.listen(port);
+}).catch(err => {
+    console.log(err);
+})
